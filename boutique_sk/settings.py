3a49bc0df87b16b943457f60bad3009ffc956270
isiko1 <lutersweg@hotmail.com>
2020-07-06 17:11:36 +0000

    056 deployment part 2

--- boutique_sk/settings.py
+++ boutique_sk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'k-&h#38^s=le%4q*q#a*@kvfrc0*#i=lk49k7%*#c=*utrdzcn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique-sk.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,13 +119,17 @@ WSGI_APPLICATION = 'boutique_sk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
